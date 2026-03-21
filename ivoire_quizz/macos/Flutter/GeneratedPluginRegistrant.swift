4325b66ea35723ba7b2a1ad0dc0adfda27300354
shikileliondor <morelyann10@gmail.com>
2026-03-21 13:58:46 +0000

    début de travail avec lancement flutter

--- ivoire_quizz/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ivoire_quizz/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,13 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus
-import flutter_secure_storage_macos
+import flutter_secure_storage_darwin
 import google_sign_in_ios
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
-  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
